596cf88cfa63a53f16a6cff3c5ad6536348cb493
Mateo <mateo55@live.com>
2025-02-10 12:27:16 +0100

    Removes API Key

--- DreiWeather/DreiWeather/Utils/Constants.swift
+++ DreiWeather/DreiWeather/Utils/Constants.swift
@@ -11,7 +11,7 @@ import SwiftUI
 enum Constants {
     enum API {
         static let baseURL = "https://api.openweathermap.org/data/2.5/weather"
-        static let apiKey = "f5cb0b965ea1564c50c6f1b74534d823"
+        static let apiKey = {YOUR_API_KEY}
         static let iconBaseURL = "https://openweathermap.org/img/wn"
     }
     
